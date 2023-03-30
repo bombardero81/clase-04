c8ce8328318dd93c27225d59fa2cc2893e760017
Pablo Godoy <pgodoyrobles@gmail.com>
2023-03-29 23:08:47 -0300

    primer commit

---
+++ src/app/directives/bloque-redondeado/bloque-redondeado.directive.spec.ts
@@ -0,0 +1,8 @@
+import { BloqueRedondeadoDirective } from './bloque-redondeado.directive';
+
+describe('BloqueRedondeadoDirective', () => {
+  it('should create an instance', () => {
+    const directive = new BloqueRedondeadoDirective();
+    expect(directive).toBeTruthy();
+  });
+});
---
+++ src/app/directives/bloque-redondeado/bloque-redondeado.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BloqueRedondeadoDirective } from './bloque-redondeado.directive';
+
+
+
+@NgModule({
+  declarations: [
+    BloqueRedondeadoDirective
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class BloqueRedondeadoModule { }
